e47946e2afdf594ab05570832fb61cbf506925ea
msrutiv <msrutiv@gmail.com>
2014-01-31 22:17:41 -0500

    Initial commit

---
+++ src/com/example/LetsMeat2/MyActivity.java
@@ -0,0 +1,37 @@
+package com.example.LetsMeat2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import com.facebook.LoggingBehavior;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Settings;
+import com.facebook.model.GraphUser;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+        Settings.addLoggingBehavior(LoggingBehavior.REQUESTS);
+
+        Request request = Request.newGraphPathRequest(null, "/4", new Request.Callback() {
+            @Override
+            public void onCompleted(Response response) {
+                if(response.getError() != null) {
+                    Log.i("MainActivity", String.format("Error making request: %s", response.getError()));
+                } else {
+                    GraphUser user = response.getGraphObjectAs(GraphUser.class);
+                    Log.i("MainActivity", String.format("Name: %s", user.getName()));
+                }
+            }
+        });
+        request.executeAsync();
+    }
+}
